92e1939c6db1023377f155ff0d5a97dce16f7ad8
Amit <amityadav101508@gmail.com>
2025-08-21 00:18:28 +0530

    nothing

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,15 +15,14 @@ const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true, // allow frontend to send cookies
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({ Hello: "test123" });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
